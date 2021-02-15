3bbd590b5d5ad43de3492c4ae5b817e005dfc0b3
Marina-Kr <mskrjk@mail.ru>
2021-02-15 23:58:29 +0300

    added hw (1 table)

--- src/main/java/ru/kryuchkova/dao/AuthorDAO.java
+++ src/main/java/ru/kryuchkova/dao/AuthorDAO.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.kryuchkova.model.Author;
-
 import java.util.List;
 import java.util.Optional;
 
